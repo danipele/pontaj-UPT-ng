6785258da04ccc38c0444490e33423b8233f3220
Daniel Pele <daniel.pele@unifiedpost.com>
2021-06-06 21:39:19 +0300

    fix code and bugs

--- src/app/components/calendar-header/calendar-header.component.spec.ts
+++ src/app/components/calendar-header/calendar-header.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalendarHeaderComponent } from './calendar-header.component';
 
 describe('CalendarHeaderComponent', () => {
